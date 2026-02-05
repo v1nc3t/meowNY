03f1bd70bb43cb0b1706d23ab1d0103476fbfe3c
vincent <vinayakgeorgeciobanu@gmail.com>
2026-02-05 12:59:30 +0100

    build: updated version on server

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan(basePackages = { "commons", "server" })
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 }
