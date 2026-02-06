6f00e404f2a7297fb932f3922654e67f9715ca1d
vincent <vinayakgeorgeciobanu@gmail.com>
2026-02-06 22:58:30 +0100

    feat: made main app resizable and set min bounds

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -1,5 +1,6 @@
 package client.scenes;
 
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Screen;
@@ -10,10 +11,11 @@ public class MainCtrl {
 
     private Stage primaryStage;
 
-    public void initialize(Stage primaryStage) {
     private ApplicationCtrl applicationCtrl;
     private Scene applicationScene;
 
+    public static final double MIN_RATIO = 0.40;
+    public static final double DEFAULT_RATIO = 0.60;
 
     public void initialize(Stage primaryStage, Pair<ApplicationCtrl, Parent> applicationOverview) {
         this.primaryStage = primaryStage;
@@ -21,11 +23,45 @@ public class MainCtrl {
         this.applicationCtrl = applicationOverview.getKey();
         this.applicationScene = new Scene(applicationOverview.getValue());
 
+        setInitialSize();
         
+        setupResizing();
 
         showApplication();
         primaryStage.show();
     }
+
+    private void setInitialSize() {
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        double screenW = screenBounds.getWidth();
+        double screenH = screenBounds.getHeight();
+        primaryStage.setWidth(screenW * DEFAULT_RATIO);
+        primaryStage.setHeight(screenH * DEFAULT_RATIO);
+    }
+
+    private void setupResizing() {
+        primaryStage.xProperty().addListener(
+                (observable, oldValue, newValue) -> updateMinConstraints()
+        );
+        primaryStage.yProperty().addListener(
+                (observable, oldValue, newValue) -> updateMinConstraints()
+        );
+
+        updateMinConstraints();
+    }
+
+    private void updateMinConstraints() {
+        var screens = Screen.getScreensForRectangle(
+                primaryStage.getX(), primaryStage.getY(), primaryStage.getWidth(), primaryStage.getHeight()
+        );
+
+        if (!screens.isEmpty()) {
+            Rectangle2D bounds = screens.getFirst().getVisualBounds();
+            primaryStage.setMinWidth(bounds.getWidth() * MIN_RATIO);
+            primaryStage.setMinHeight(bounds.getHeight() * MIN_RATIO);
+        }
+    }
+
     public void showApplication() {
         primaryStage.setTitle("meowNY - Expense Tracker");
         primaryStage.setScene(applicationScene);
