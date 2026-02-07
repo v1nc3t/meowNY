235645575bc944ea43a4c5a7df772849d3557618
vincent <vinayakgeorgeciobanu@gmail.com>
2026-02-07 14:12:10 +0100

    feat: completed the main application structure

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -20,7 +20,6 @@ public class Main extends Application {
         launch();
     }
 
-    @SuppressWarnings("checkstyle:MagicNumber")
     @Override
     public void start(Stage primaryStage) throws Exception {
 
