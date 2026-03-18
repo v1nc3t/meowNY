6da034acd338e021a7f37446762863410e13a579
vincent <vinayakgeorgeciobanu@gmail.com>
2026-03-18 18:22:01 +0100

    feat: added budget and tests

---
+++ commons/src/main/java/commons/Budget.java
@@ -0,0 +1,159 @@
+package commons;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "budgets", uniqueConstraints = {
+        @UniqueConstraint(
+                name =  "unique_user_category_period",
+                columnNames = {"user_id", "expense_category_id", "month", "year"}
+        )
+})
+public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    @PositiveOrZero
+    private Double limitAmount;
+
+    @Column(nullable = false)
+    @Min(1)
+    @Max(12)
+    private Integer month;
+
+    @Column(nullable = false)
+    @Min(1923)
+    @Max(9999)
+    private Integer year;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "expense_category_id", nullable = false)
+    private ExpenseCategory category;
+
+    /**
+     * Empty constructor for object mapper
+     */
+    public Budget() {
+    }
+
+    public Budget(
+            Double limitAmount,
+            Integer month,
+            Integer year,
+            ExpenseCategory category
+    ) {
+        if (limitAmount == null || limitAmount < 0) {
+            throw new IllegalArgumentException("Limit amount must be positive.");
+        }
+        if (month == null || month < 1 || month > 12) {
+            throw new IllegalArgumentException("Month must be between 1 and 12.");
+        }
+        if (year == null || year < 1923 || year > 9999) {
+            throw  new IllegalArgumentException("Year must be between 1923 and 9999.");
+        }
+        if (category == null) {
+            throw new IllegalArgumentException("Category must exist to create budget.");
+        }
+        this.limitAmount = limitAmount;
+        this.month = month;
+        this.year = year;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Double getLimitAmount() {
+        return limitAmount;
+    }
+
+    public void setLimitAmount(Double limitAmount) {
+        if (limitAmount == null || limitAmount < 0) {
+            throw new IllegalArgumentException("Limit amount must be positive.");
+        }
+        this.limitAmount = limitAmount;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        if (month == null || month < 1 || month > 12) {
+            throw new IllegalArgumentException("Month must be between 1 and 12.");
+        }
+        this.month = month;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        if (year == null || year < 1923 || year > 9999) {
+            throw  new IllegalArgumentException("Year must be between 1923 and 9999.");
+        }
+        this.year = year;
+    }
+
+    public ExpenseCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ExpenseCategory category) {
+        if (category == null) {
+            throw new IllegalArgumentException("Category must exist to create budget.");
+        }
+        this.category = category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Budget budget = (Budget) o;
+        return Objects.equals(user, budget.user)
+                && Objects.equals(limitAmount, budget.limitAmount)
+                && Objects.equals(month, budget.month)
+                && Objects.equals(year, budget.year)
+                && Objects.equals(category, budget.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, limitAmount, month, year, category);
+    }
+
+    @Override
+    public String toString() {
+        return "Budget{" +
+                "id=" + id +
+                ", limitAmount=" + limitAmount +
+                ", month=" + month +
+                ", year=" + year +
+                ", category=" + category +
+                '}';
+    }
+}
---
+++ commons/src/test/java/commons/BudgetTest.java
@@ -0,0 +1,142 @@
+package commons;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BudgetTest {
+
+    private Budget testBudget1;
+    private Budget testBudget2;
+    private Budget testBudget3;
+
+    private final ExpenseCategory testCategory = new ExpenseCategory("food");
+    private final User testUser = new User();
+
+    @BeforeEach
+    void setUp() {
+        testBudget1 = new Budget(50.0, 11, 2000, testCategory);
+        testBudget2 = new Budget(100.0, 4, 2010, testCategory);
+        testBudget3 = new Budget(100.0, 4, 2010, testCategory);
+    }
+
+    @Test
+    void invalidConstructorTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+           Budget invalidBudget = new Budget(-1.0, 10, 2000, testCategory);
+        }, "invalid limit");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+           Budget invalidBudget = new Budget(1.0, 13, 2000, testCategory);
+        }, "invalid month");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+           Budget invalidBudget = new Budget(1.0, 10, 10000, testCategory);
+        }, "invalid year");
+
+        assertThrows(IllegalArgumentException.class, () -> {
+           Budget invalidBudget = new Budget(1.0, 10, 2000, null);
+        }, "not null category");
+    }
+
+    @Test
+    void setIdTest() {
+        Long id = 1L;
+        testBudget1.setId(id);
+        assertEquals(id, testBudget1.getId());
+    }
+
+    @Test
+    void setUserTest() {
+        User testUser = new User();
+        testBudget1.setUser(testUser);
+        assertEquals(testUser, testBudget1.getUser());
+    }
+
+    @Test
+    void setValidLimitAmountTest() {
+        double limit = 100.0;
+        testBudget1.setLimitAmount(limit);
+        assertEquals(limit, testBudget1.getLimitAmount());
+    }
+
+    @Test
+    void setInvalidLimitAmountTest() {
+        double limit = -100.0;
+        assertThrows(IllegalArgumentException.class, () -> {
+            testBudget1.setLimitAmount(limit);
+        });
+    }
+
+    @Test
+    void setValidMonthTest() {
+        int month = 5;
+        testBudget1.setMonth(month);
+        assertEquals(month, testBudget1.getMonth());
+    }
+
+    @Test
+    void setInvalidMonthTest() {
+        int month = 13;
+        assertThrows(IllegalArgumentException.class, () -> {
+            testBudget1.setMonth(month);
+        });
+    }
+
+    @Test
+    void setValidYearTest() {
+        int year = 1950;
+        testBudget1.setYear(year);
+        assertEquals(year, testBudget1.getYear());
+    }
+
+    @Test
+    void setInvalidYearTest() {
+        int year = 1900;
+        assertThrows(IllegalArgumentException.class, () -> {
+            testBudget1.setYear(year);
+        });
+    }
+
+    @Test
+    void setNullCategory() {
+        ExpenseCategory newCategory = new ExpenseCategory("travel");
+        testBudget1.setCategory(newCategory);
+        assertEquals(newCategory, testBudget1.getCategory());
+    }
+
+    @Test
+    void setNotNullCategory() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            testBudget1.setCategory(null);
+        });
+    }
+
+    @Test
+    void testNotEquals() {
+        assertNotEquals(testBudget1, testBudget2);
+    }
+
+    @Test
+    void testEquals() {
+        assertEquals(testBudget3, testBudget2);
+    }
+
+    @Test
+    void testHashCodeDiff() {
+        assertNotEquals(testBudget1.hashCode(), testBudget2.hashCode());
+    }
+
+    @Test
+    void testHashCodeSame() {
+        assertEquals(testBudget2.hashCode(), testBudget3.hashCode());
+    }
+
+    @Test
+    void testToString() {
+        String expected = "Budget{id=null, limitAmount=100.0, month=4, year=2010, category=" + testCategory.toString() + "}";
+        assertEquals(expected, testBudget2.toString());
+    }
+}
