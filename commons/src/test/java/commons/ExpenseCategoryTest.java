da576284d6ab4e59477b0a0a3847f568e0a3c5dd
vincent <vinayakgeorgeciobanu@gmail.com>
2026-03-18 18:37:54 +0100

    feat: add expense category and tests

---
+++ commons/src/test/java/commons/ExpenseCategoryTest.java
@@ -0,0 +1,66 @@
+package commons;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExpenseCategoryTest {
+
+    private ExpenseCategory testExpenseCategory1;
+    private ExpenseCategory testExpenseCategory2;
+    private ExpenseCategory testExpenseCategory3;
+
+    @BeforeEach
+    void setUp() {
+        testExpenseCategory1 = new ExpenseCategory("food");
+        testExpenseCategory2 = new ExpenseCategory("car");
+        testExpenseCategory3 = new ExpenseCategory("car");
+    }
+
+    @Test
+    void invalidConstructorTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            ExpenseCategory invalidTestExpenseCategory = new ExpenseCategory(null);
+        }, "invalid category name");
+    }
+
+    @Test
+    void setIdTest() {
+        Long id = 1L;
+        testExpenseCategory1.setId(id);
+        assertEquals(id, testExpenseCategory1.getId());
+    }
+
+    @Test
+    void setValidCategoryName() {
+        String newCategory = "water";
+        testExpenseCategory1.setCategoryName(newCategory);
+        assertEquals(newCategory, testExpenseCategory1.getCategoryName());
+    }
+
+    @Test
+    void setInvalidCategoryName() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            testExpenseCategory1.setCategoryName(null);
+        }, "invalid category name");
+    }
+
+    @Test
+    void notEqualsTest() {
+        assertNotEquals(testExpenseCategory1, testExpenseCategory2);
+    }
+
+    @Test
+    void equalsTest() {
+        assertEquals(testExpenseCategory3, testExpenseCategory2);
+    }
+
+    @Test
+    void hashCodeDiffTest() {
+        assertNotEquals(testExpenseCategory1.hashCode(), testExpenseCategory2.hashCode());
+    }
+    @Test
+    void hashCodeSameTest() {
+        assertEquals(testExpenseCategory3.hashCode(), testExpenseCategory2.hashCode());
+    }
+}
