251e93fae39405b3002d303f61d4387f29a747b2
vincent <vinayakgeorgeciobanu@gmail.com>
2026-03-19 13:50:43 +0100

    feat: add income category and tests

---
+++ commons/src/test/java/commons/IncomeCategoryTest.java
@@ -0,0 +1,68 @@
+package commons;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IncomeCategoryTest {
+
+    private IncomeCategory testIncomeCategory1;
+    private IncomeCategory testIncomeCategory2;
+    private IncomeCategory testIncomeCategory3;
+
+    @BeforeEach
+    void setUp() {
+        testIncomeCategory1 = new IncomeCategory("rent");
+        testIncomeCategory2 = new IncomeCategory("car");
+        testIncomeCategory3 = new IncomeCategory("car");
+    }
+
+    @Test
+    void invalidConstructorTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            IncomeCategory invalidTestIncomeCategory = new IncomeCategory(null);
+        }, "invalid category name");
+    }
+
+    @Test
+    void setIdTest() {
+        Long id = 1L;
+        testIncomeCategory1.setId(id);
+        assertEquals(id, testIncomeCategory1.getId());
+    }
+
+    @Test
+    void setValidCategoryNameTest() {
+        String newCategory = "hustle";
+        testIncomeCategory1.setCategoryName(newCategory);
+        assertEquals(newCategory, testIncomeCategory1.getCategoryName());
+    }
+
+    @Test
+    void setInvalidCategoryNameTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            testIncomeCategory1.setCategoryName(null);
+        }, "invalid category name");
+    }
+
+    @Test
+    void notEqualsTest() {
+        assertNotEquals(testIncomeCategory1, testIncomeCategory2);
+    }
+
+    @Test
+    void equalsTest() {
+        assertEquals(testIncomeCategory3, testIncomeCategory2);
+    }
+
+    @Test
+    void hashCodeDiffTest() {
+        assertNotEquals(testIncomeCategory1.hashCode(), testIncomeCategory2.hashCode());
+    }
+
+    @Test
+    void hashCodeSameTest() {
+        assertEquals(testIncomeCategory3.hashCode(), testIncomeCategory2.hashCode());
+    }
+
+}
