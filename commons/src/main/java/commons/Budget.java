9e219e9993ca0b8211565a7b9b693306fcf08dd8
vincent <vinayakgeorgeciobanu@gmail.com>
2026-03-19 18:08:53 +0100

    fix: improve classes

--- commons/src/main/java/commons/Budget.java
+++ commons/src/main/java/commons/Budget.java
@@ -18,7 +18,7 @@ public class Budget {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
--- commons/src/main/java/commons/User.java
+++ commons/src/main/java/commons/User.java
@@ -3,7 +3,6 @@ package commons;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
