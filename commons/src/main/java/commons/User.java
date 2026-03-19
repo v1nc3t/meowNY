21d21bc65d267c50ad19b50ea79b8ce20be20056
vincent <vinayakgeorgeciobanu@gmail.com>
2026-03-19 18:01:51 +0100

    feat: add user and tests

---
+++ commons/src/main/java/commons/User.java
@@ -0,0 +1,205 @@
+package commons;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String firstName;
+
+    @Column(length = 30, nullable = false)
+    private String lastName;
+
+    @Column(length = 30, nullable = false)
+    private String email;
+
+    @Column(length = 30, nullable = false)
+    private String username;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Income> incomes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Expense> expenses = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Budget> budgets = new ArrayList<>();
+
+    /**
+     * Empty constructor for object mapper
+     */
+    public User() {
+    }
+
+    public User(
+            String firstName,
+            String lastName,
+            String email,
+            String username,
+            String password
+    ) {
+        if (firstName == null) {
+            throw new IllegalArgumentException("First name can not be null.");
+        }
+        if (lastName == null) {
+            throw new IllegalArgumentException("Last name can not null.");
+        }
+        if (username == null) {
+            throw new IllegalArgumentException("Username can not be null.");
+        }
+        if (password == null) {
+            throw new IllegalArgumentException("Password can not be null.");
+        }
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName == null) {
+            throw new IllegalArgumentException("First name can not be null.");
+        }
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName == null) {
+            throw new IllegalArgumentException("Last name can not null.");
+        }
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        if (username == null) {
+            throw new IllegalArgumentException("Username can not be null.");
+        }
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password can not be null.");
+        }
+        this.password = password;
+    }
+
+    public List<Income> getIncomes() {
+        return incomes;
+    }
+
+    public void setIncomes(List<Income> incomes) {
+        this.incomes = incomes;
+    }
+
+    public void addIncome(Income income) {
+        this.incomes.add(income);
+        income.setUser(this);
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+
+    public void addExpense(Expense expense) {
+        this.expenses.add(expense);
+        expense.setUser(this);
+    }
+
+    public List<Budget> getBudgets() {
+        return budgets;
+    }
+
+    public void setBudgets(List<Budget> budgets) {
+        this.budgets = budgets;
+    }
+
+    public void addBudget(Budget budget) {
+        this.budgets.add(budget);
+        budget.setUser(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id)
+                && Objects.equals(firstName, user.firstName)
+                && Objects.equals(lastName, user.lastName)
+                && Objects.equals(email, user.email)
+                && Objects.equals(username, user.username)
+                && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                id, firstName, lastName, email, username, password
+        );
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
---
+++ commons/src/test/java/commons/UserTest.java
@@ -0,0 +1,145 @@
+package commons;
+
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserTest {
+
+    private User testUser1;
+    private User testUser2;
+    private User testUser3;
+
+    @BeforeEach
+    void setUp() {
+        testUser1 = new User("Will", "Born", "wb@gmail.com", "wlbr", "test");
+        testUser2 = new User("Max", "Smith", "ms@gmail.com", "mxSm", "test");
+        testUser3 = new User("Max", "Smith", "ms@gmail.com", "mxSm", "test");
+    }
+
+    @Test
+    void setIdTest() {
+        Long id = 1L;
+        testUser1.setId(id);
+        assertEquals(id, testUser1.getId());
+    }
+
+    @Test
+    void setValidFirstNameTest() {
+        String firstName = "Feb";
+        testUser1.setFirstName(firstName);
+        assertEquals(firstName, testUser1.getFirstName());
+    }
+
+    @Test
+    void setInvalidFirstNameTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            testUser1.setFirstName(null);
+        });
+    }
+
+    @Test
+    void setValidLastNameTest() {
+        String lastName = "Feb";
+        testUser1.setLastName(lastName);
+        assertEquals(lastName, testUser1.getLastName());
+    }
+
+    @Test
+    void setInvalidLastNameTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            testUser1.setLastName(null);
+        });
+    }
+
+    @Test
+    void getFullNameTest() {
+        assertEquals("Max Smith", testUser2.getFullName());
+    }
+
+    @Test
+    void setEmailTest() {
+        String email = "test@gmail.com";
+        testUser1.setEmail(email);
+        assertEquals(email, testUser1.getEmail());
+    }
+
+    @Test
+    void setNullEmailTest() {
+        testUser1.setEmail(null);
+        assertNull(testUser1.getEmail());
+    }
+
+    @Test
+    void setValidUsernameTest() {
+        String username = "testusr";
+        testUser1.setUsername(username);
+        assertEquals(username, testUser1.getUsername());
+    }
+
+    @Test
+    void setInvalidUsernameTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            testUser1.setUsername(null);
+        });
+    }
+
+    @Test
+    void setValidPasswordTest() {
+        String password = "incorrect";
+        testUser1.setPassword(password);
+        assertEquals(password, testUser1.getPassword());
+    }
+
+    @Test
+    void setInvalidPasswordTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            testUser1.setPassword(null);
+        });
+    }
+
+    @Test
+    void incomesListTest() {
+        List<Income> incomes = new ArrayList<>();
+        testUser1.setIncomes(incomes);
+        assertEquals(incomes, testUser1.getIncomes());
+    }
+
+    @Test
+    void expenseListTest() {
+        List<Expense> expenses = new ArrayList<>();
+        testUser1.setExpenses(expenses);
+        assertEquals(expenses, testUser1.getExpenses());
+    }
+
+    @Test
+    void budgetsListTest() {
+        List<Budget> budgets = new ArrayList<>();
+        testUser1.setBudgets(budgets);
+        assertEquals(budgets, testUser1.getBudgets());
+    }
+
+    @Test
+    void notEqualsTest() {
+        assertNotEquals(testUser1, testUser2);
+    }
+
+    @Test
+    void equalsTest() {
+        assertEquals(testUser3, testUser2);
+    }
+
+    @Test
+    void hashCodeDiffTest() {
+        assertNotEquals(testUser1.hashCode(), testUser2.hashCode());
+    }
+
+    @Test
+    void hashCodeSameTest() {
+        assertEquals(testUser3.hashCode(), testUser2.hashCode());
+    }
+
+}
