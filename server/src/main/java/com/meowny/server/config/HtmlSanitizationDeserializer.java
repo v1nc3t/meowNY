0330b1a3a9baed5e9d82c5dd2a48aa3ece047bdf
vincent <vinayakgeorgeciobanu@gmail.com>
2026-07-18 11:34:44 +0200

    feat: add html parsing for user input

---
+++ server/src/main/java/com/meowny/server/config/HtmlSanitizationDeserializer.java
@@ -0,0 +1,25 @@
+package com.meowny.server.config;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import org.jsoup.Jsoup;
+import org.jsoup.parser.Parser;
+import org.jsoup.safety.Safelist;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+
+@JsonComponent
+public class HtmlSanitizationDeserializer extends JsonDeserializer<String> {
+
+    @Override
+    public String deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+        String value = parser.getValueAsString();
+        if (value == null) return null;
+
+        String cleanValue = Jsoup.clean(value, Safelist.none());
+
+        return Parser.unescapeEntities(cleanValue, true);
+    }
+}
