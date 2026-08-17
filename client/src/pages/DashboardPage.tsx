30411bb11051765d6adabbb4f574cd8fc7906846
v1c3nt <vinayakgeorgeciobanu@gmail.com>
2026-08-17 22:06:55 +0200

    feat(pages): add dashboard and landing page

---
+++ client/src/pages/DashboardPage.tsx
@@ -0,0 +1,19 @@
+export default function DashboardPage() {
+    return (
+      <div>
+        <h1>Dashboard View (Mock)</h1>
+        <p>Welcome to the meowNY app viewer.</p>
+        
+        <div>
+          <div>
+            <h4>Total Balance</h4>
+            <p>$2,450.00</p>
+          </div>
+          <div>
+            <h4>Monthly Budget</h4>
+            <p>$1,200.00</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,23 @@
+export default function LandingPage() {
+    return (
+        <main>
+            <h1>Welcome to meowNY</h1>
+            <p>Manage your expenses, income, and budgets easily.</p>
+
+            <section>
+                <div>
+                    <h3>Expenses</h3>
+                    <p>Track your expenses, and expenses that occur regurarly.</p>
+                </div>
+                <div>
+                    <h3>Income</h3>
+                    <p>Track your income, and set it automatically.</p>
+                </div>
+                <div>
+                    <h3>Budgets</h3>
+                    <p>Monitor your active category and global limits.</p>
+                </div>
+        </section>
+        </main>
+    )
+}
